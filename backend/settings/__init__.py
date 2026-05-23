32e0ecbfe92e5797d3ea970cdf333b0f7543d91b
James Bond <111063694+jhuscoder@users.noreply.github.com>
2026-05-23 17:42:28 +0100

    Install API app

--- backend/settings/__init__.py
+++ backend/settings/__init__.py
@@ -1,8 +1,5 @@
 from .configuration import *
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
@@ -19,6 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
