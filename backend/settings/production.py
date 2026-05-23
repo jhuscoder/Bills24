952f117983afe93cae21a6b10fb730ea6d8beb5c
James Bond <111063694+jhuscoder@users.noreply.github.com>
2026-05-23 20:11:28 +0100

    updated

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -25,8 +25,3 @@ LOGGING = {
 # REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
 #     'backend.middleware.EncryptedJSONRenderer',
 # )
-
-HEALTH_CHECK = {
-    'DISK_USAGE_MAX': 90,  # percent
-    'MEMORY_MIN': 100,    # in MB
-}
