d65cc323d8a44fd99b54917f358378e1194c035b
James Bond <111063694+jhuscoder@users.noreply.github.com>
2026-05-23 18:56:22 +0100

    Django Health

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 from django.urls import path, include
+from health_check.views import HealthCheckView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf.urls import i18n
+from django.conf import settings
+
 
 urlpatterns = [
     path('', include('api.urls')),
+    path(
+        "health/",
+        HealthCheckView.as_view(
+            checks=[  
+                # optional, default is all but 3rd party checks
+                "health_check.Cache",
+                # "health_check.DNS",
+                "health_check.Database",
+                "health_check.Mail",
+                "health_check.Storage",
+                 # 3rd party checks
+                "health_check.contrib.psutil.CPU",
+                "health_check.contrib.psutil.Memory",
+                "health_check.contrib.psutil.Disk",
+                "health_check.contrib.psutil.Memory",
+            ]
+        )
+    ),
+    path('i18n/', include(i18n))
 ]
